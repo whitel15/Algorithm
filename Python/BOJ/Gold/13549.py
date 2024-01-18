1b9ee751dbbec5199c0dae012dc29b0ec6801948
jieun <jieunbaek1@naver.com>
2024-01-18 09:37:15 +0900

    DFS/BFS

---
+++ Python/BOJ/Gold/13549.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+visited = [0 for _ in range(100001)]
+count = 0
+ans = 0
+dq = deque()
+dq.append((n, count))
+
+while dq:
+    locate, cnt = dq.popleft()
+
+    if locate == k and visited[locate] == 0:
+        ans = cnt
+        break
+
+    visited[locate] = 1
+
+    if 0 <= locate * 2 < 100001 and visited[locate * 2] == 0 and locate < k:
+        dq.appendleft((locate * 2, cnt))
+    if 0 <= locate - 1 < 100001 and visited[locate - 1] == 0:
+        dq.append((locate - 1, cnt + 1))
+    if 0 <= locate + 1 < 100001 and visited[locate + 1] == 0:
+        dq.append((locate + 1, cnt + 1))
+
+print(ans)
