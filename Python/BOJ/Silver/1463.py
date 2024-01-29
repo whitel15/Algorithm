374eb18e24767410ac031cbbdaa3c6626c8373c5
jieun <jieunbaek1@naver.com>
2024-01-29 16:31:09 +0900

    DP

---
+++ Python/BOJ/Silver/1463.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(1111111)
+
+x = int(input())
+
+dp = {1:0}
+
+def rec(n):
+    if n in dp:
+        return dp[n]
+    if n % 3 == 0 and n % 2 == 0:
+        dp[n] = min(rec(n//3) + 1, rec(n//2) + 1)
+    elif n % 3 == 0:
+        dp[n] = min(rec(n//3) + 1, rec(n-1) + 1)
+    elif n % 2 == 0:
+        dp[n] = min(rec(n//2) + 1, rec(n-1) + 1)
+    else:
+        dp[n] = rec(n-1) + 1
+    return dp[n]
+
+print(rec(x))
