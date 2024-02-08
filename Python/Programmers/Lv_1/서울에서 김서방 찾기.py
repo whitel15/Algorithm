2c7bba84ca4a546a6ceec0e924825aee8041fb20
jieun <jieunbaek1@naver.com>
2024-02-08 15:42:12 +0900

    solve LV1

---
+++ Python/Programmers/Lv_1/서울에서 김서방 찾기.py
@@ -0,0 +1,5 @@
+def solution(seoul):
+    
+    answer = "김서방은 " + str(seoul.index("Kim")) + "에 있다"
+
+    return answer
---
+++ Python/Programmers/Lv_1/콜라츠 추측.py
@@ -0,0 +1,12 @@
+def solution(num):
+    answer = -1
+
+    for i in range(500):
+        if num == 1:
+            return i
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num = num * 3 + 1
+
+    return answer
